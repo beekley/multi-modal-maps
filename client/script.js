dd7cb059cb24f27f0f713a92e9f0b239e41f3359
Brett Beekley <brettbeekley@gmail.com>
2017-07-05 11:08:22 -0700

    Moving dr and ds to rendering functions

--- client/script.js
+++ client/script.js
@@ -1,17 +1,48 @@
-console.log('hello!');
+// Dev case
+const origin = '2050 S Bentley Ave Los Angeles 90025';
+const dest = '1906 Ocean Ave, Santa Monica, CA';
 
 function initMap() {
-  console.log(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+  const ds = new google.maps.DirectionsService;
+  const dr = new google.maps.DirectionsRenderer;
+  //const dr2 = new google.maps.DirectionsRenderer;
+  
+  // Updates map with given directions
+  const onChangeHandler = function() {
+    calculateAndDisplayRoute(origin, dest, ds, dr);
+    //calculateAndDisplayRoute('2050 S Bentley Ave Los Angeles 90025', '5300 Beethoven Street, Los Angeles, CA 90066', ds, dr2);
+  };
+  
+  // Render the map
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 41.85, lng: -87.65}
   });
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+  dr.setMap(map);
+  //dr2.setMap(map);
+  onChangeHandler();
+}
+
+function calculateAndDisplayRoute(origin, destination, ds, dr) {
+  ds.route({
+    origin,
+    destination,
+    travelMode: 'TRANSIT'
+  }, function(response, status) {
+    if (status === 'OK') {
+      console.log(response);
+      getWalkingSteps(response)
+      dr.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
   });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
+}
+
+function getWalkingSteps(response) {
+  const walkingSteps = response.routes[0].legs[0].steps.filter(step => {
+    return step.travel_mode === 'WALKING';
   });
+  
+  console.log(walkingSteps);
 }
