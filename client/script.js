69ff4e6ef50e4dd583d64fbda644c986157dc8f8
Brett Beekley <brettbeekley@gmail.com>
2017-07-05 15:04:40 -0700

    Added alt route getter, but doesnt recurse yet

--- client/script.js
+++ client/script.js
@@ -1,23 +1,25 @@
 // Dev case
-const origin = '2050 S Bentley Ave Los Angeles 90025';
-const dest = '1906 Ocean Ave, Santa Monica, CA';
-const alt = '5300 Beethoven Street, Los Angeles, CA 90066';
+//const origin = '2050 S Bentley Ave Los Angeles 90025';
+//const dest = '1906 Ocean Ave, Santa Monica, CA';
+//const alt = '5300 Beethoven Street, Los Angeles, CA 90066';
+const verbose = false;
+let map;
 
 function initMap() {
   
-  // Updates map with given directions
-  const onChangeHandler = function() {
-    calculateAndDisplayRoute(origin, dest, map);
-  };
-  
   // Render the map
-  const map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
     center: {lat: 41.85, lng: -87.65}
   });
-  //dr2.setMap(map);
-  onChangeHandler();
+  
+  //onChangeHandler();
 }
+
+// Updates map with given directions
+const onChangeHandler = function() {
+  calculateAndDisplayRoute(document.getElementById('origin').value, document.getElementById('destination').value, map);
+};
 
 function calculateAndDisplayRoute(origin, destination, map) {
   
@@ -33,27 +35,17 @@ function calculateAndDisplayRoute(origin, destination, map) {
     async (response, status) => {
       if (status === 'OK') {
         
-        // Initialize current time as current time
-        let departure_time = new Date();
+        // Get routes
+        const routes = await getRoutes(response);
         
-        // Intialize empty array of new routes
-        const routes = [];
+        // Get routes, preferring rail. Run again to replace walking on new route
+        const railRoutes = await getRoutes(response, {
+          modes: ['RAIL','SUBWAY','TRAIN','TRAM'],
+          routingPreference: 'FEWER_TRANSFERS'
+        });
         
-        // Iterate through steps in response
-        for (step of response.routes[0].legs[0].steps) {
-          // If the travel_mode is walking, set travel mode to biking
-          const travelMode = step.travel_mode === 'WALKING' ? 'BICYCLING' : 'TRANSIT';
-        
-          // Push route to routes
-          try {
-            routes.push(await getRoute(step.start_location, step.end_location, departure_time, travelMode));
-          } 
-          catch (err) {
-            console.log(err);
-          }
-        }
-        
-        console.log(routes);
+        console.log(railRoutes);
+        console.log('Alt duration (mins)', getDuration(railRoutes));
         
         // Plot routes
         routes.forEach(route => {
@@ -62,14 +54,18 @@ function calculateAndDisplayRoute(origin, destination, map) {
           dr.setDirections(route);
         });
         
-        // Get durations
-        console.log('Original duration (mins)', Math.round(response.routes[0].legs.reduce((dur, leg) => {
-          return dur + leg.duration.value;
-        }, 0) / 60));
+        // Update map
+        // Something is asynchronouse above this that's causing problems...
+        setTimeout(() => {showRoutes(response, map)}, 0);
         
-        console.log('New duration (mins)', Math.round(routes.reduce((dur, route) => {
-          return dur + route.routes[0].legs[0].duration.value;
-        }, 0) / 60));
+        // Get durations
+        const originalDuration = Math.round(response.routes[0].legs.reduce((dur, leg) => {
+          return dur + leg.duration.value;
+        }, 0) / 60);
+        const newDuration = getDuration(routes);
+        
+        console.log('Original duration (mins)', originalDuration);
+        console.log('New duration (mins)', newDuration);
         
       } else {
         window.alert('Directions request failed due to ' + status);
@@ -78,10 +74,36 @@ function calculateAndDisplayRoute(origin, destination, map) {
   );
 }
 
-function getWalkingSteps(response) {
-  return response.routes[0].legs[0].steps.filter(step => {
-    return step.travel_mode === 'WALKING';
-  });
+// Returns a duration (in mins) for the given set of routes
+const getDuration = (routes) => {
+  return Math.round(routes.reduce((dur, route) => {
+    return dur + route.routes[0].legs[0].duration.value;
+  }, 0) / 60);
+}
+
+// Returns an array of routes
+const getRoutes = async (response, transitOptions) => {
+  // Intialize empty array of new routes
+  const routes = [];
+
+  // Iterate through steps in response
+  for (step of response.routes[0].legs[0].steps) {
+    // If the travel_mode is walking, set travel mode to biking
+    const travelMode = step.travel_mode === 'WALKING' ? 'BICYCLING' : 'TRANSIT';
+
+    // Push route to routes
+    try {
+      
+      // 
+      
+      routes.push(await getRoute(step.start_location, step.end_location, travelMode, transitOptions));
+    } 
+    catch (err) {
+      throw(err);
+    }
+  }
+  
+  return routes;
 }
 
 
@@ -92,7 +114,7 @@ function getWalkingSteps(response) {
   // transit_mode (string, 'BICYCLING' or 'TRANSIT');
 // Output:
   // promise that resolves to a route
-const getRoute = (origin, destination, departure_time, travelMode) => {
+const getRoute = (origin, destination, travelMode, transitOptions) => {
   // Get default transit route
   const ds = new google.maps.DirectionsService;
   
@@ -102,13 +124,30 @@ const getRoute = (origin, destination, departure_time, travelMode) => {
       origin,
       destination,
       travelMode,
-      //transit_routing_preference: 'fewer_transfers',
+      transitOptions
     }
     
     ds.route(request, (response, status) => {
+    
+      if (verbose) console.log(`Getting ${travelMode} leg:`, request);
+      if (verbose) console.log('Response:', response);
+      
       if (status === 'OK') resolve(response);
       else reject(response);
     });
     
   });
+}
+
+// Input: 
+  // Array of routes
+  // Current map
+// Output:
+  // Map is zoomed to show entire route
+const showRoutes = (response, map) => {
+  const bounds = new google.maps.LatLngBounds();
+  bounds.extend(response.routes[0].legs[0].start_location);
+  bounds.extend(response.routes[0].legs[0].end_location);
+  console.log(bounds);
+  map.fitBounds(bounds);
 }
